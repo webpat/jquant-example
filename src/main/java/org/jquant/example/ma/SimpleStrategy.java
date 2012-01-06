df4f37606cfa088ef06f7cb6b9d6c32058889592
webpat <patrick.merheb@gmail.com>
2012-01-06 15:38:06 +1100

    Welcome to the indicators + clean
    Change-Id: Ia120513ce9c81bc25b9a35726e835914a3e9cef4

--- src/main/java/org/jquant/example/ma/SimpleStrategy.java
+++ src/main/java/org/jquant/example/ma/SimpleStrategy.java
@@ -1,31 +1,33 @@
 package org.jquant.example.ma;
 
 import org.jquant.data.Symbols;
-import org.jquant.model.Candle;
 import org.jquant.model.Symbol;
+import org.jquant.serie.Candle;
 import org.jquant.strategy.AbstractStrategy;
 import org.jquant.strategy.Strategy;
 
 @Strategy(value="Simple Strategy")
 public class SimpleStrategy extends AbstractStrategy {
 
+	
+	
 	@Override
 	public void init() {
 		
-		System.out.println("I've been called");
+		
 
 	}
 
 	@Override
 	public void createUniverse() {
-		addInstrument(Symbols.ALCATEL);
-		addInstrument(Symbols.MSFT);
+		addInstrument(Symbols.HEINZ);
+		addInstrument(Symbols.IBM);
 
 	}
 
 	@Override
 	public void onCandle(Symbol instrument, Candle candle) {
-		System.out.println("I've been called for Instrument " + instrument);
+		System.out.println("I've been called for Instrument " + instrument + " with candle " + candle + " at " + candle.getDate());
 		
 	}
 
