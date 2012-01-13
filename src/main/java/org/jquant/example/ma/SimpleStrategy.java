90d168a7cc5cd59191044dfc5544b1957641a62e
webpat <patrick.merheb@gmail.com>
2012-01-13 13:16:46 +1100

    Indicator/TimeSeries OK
    Change-Id: I922ce6a676045fc92926bb6464d1db9dfc063016

--- src/main/java/org/jquant/example/ma/SimpleStrategy.java
+++ src/main/java/org/jquant/example/ma/SimpleStrategy.java
@@ -1,23 +1,44 @@
 package org.jquant.example.ma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jquant.data.Symbols;
+import org.jquant.indicator.SMA;
 import org.jquant.model.Symbol;
 import org.jquant.serie.Candle;
+import org.jquant.serie.Candle.CandleData;
 import org.jquant.strategy.AbstractStrategy;
 import org.jquant.strategy.Strategy;
 
 @Strategy(value="Simple Strategy")
 public class SimpleStrategy extends AbstractStrategy {
 
+	private static final int MA_LENGTH = 50;
+	
+	/**
+	 * My indicators collection 
+	 */
+	Map<Symbol,SMA> smaList = new HashMap<Symbol,SMA>() ;
 	
 	
 	@Override
 	public void init() {
 		
+		/**
+		 * Init Indicators 
+		 */
+		for (Symbol s : getUniverse()){
+			SMA sma = new SMA(getCandleSerie(s), MA_LENGTH, CandleData.CLOSE);
+			smaList.put(s, sma);
+		}
 		
 
 	}
 
+	/**
+	 * The Strategy universe
+	 */
 	@Override
 	public void createUniverse() {
 		addInstrument(Symbols.HEINZ);
@@ -27,7 +48,16 @@ public class SimpleStrategy extends AbstractStrategy {
 
 	@Override
 	public void onCandle(Symbol instrument, Candle candle) {
-		System.out.println("I've been called for Instrument " + instrument + " with candle " + candle + " at " + candle.getDate());
+		
+		//logger.debug("I've been called for Instrument " + instrument + " with candle " + candle + " at " + candle.getDate());
+		
+		System.out.printf(	"name : %1$s \t " +
+							"date : %2$td-%2$tm-%2$tY \t " +
+							"close: %3$f \t" +
+							"sma :  %4$f \n"
+							
+							, instrument,candle.getDate().toDate(), candle.getClose(),smaList.get(instrument).getValue(candle.getDate()) );
+		
 		
 	}
 
