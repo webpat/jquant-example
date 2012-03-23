0bbfb97b3933042ae98ed6a44aa43466774cabdb
webpat <patrick.merheb@gmail.com>
2012-03-23 16:43:55 +1100

    PaperBroker

--- src/main/java/org/jquant/example/StrategyRunnerConfiguration.java
+++ src/main/java/org/jquant/example/StrategyRunnerConfiguration.java
@@ -1,8 +1,11 @@
 package org.jquant.example;
 
 import org.joda.time.DateTime;
+import org.jquant.execution.IBroker;
+import org.jquant.execution.PaperBroker;
 import org.jquant.model.Currency;
 import org.jquant.model.MarketDataPrecision;
+import org.jquant.portfolio.Portfolio;
 import org.jquant.strategy.StrategyRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +18,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StrategyRunnerConfiguration {
 
+	@Bean
+	public IBroker broker(){
+		
+		IBroker broker = new PaperBroker(0, 0, 0);
+		return broker;
+	}
+	
 	
 	@Bean
 	public StrategyRunner runner(){
 		
 		DateTime debut = new DateTime("2005-03-25");
 		DateTime fin = new DateTime();
-		StrategyRunner runner = new StrategyRunner(debut,fin,Currency.EUR,MarketDataPrecision.CANDLE);
+		
+
+		Portfolio ptf = new Portfolio("My strategy portfolio", Currency.USD);
+		/*
+		 * Init Portfolio with 10 000 USD cash 
+		 */
+		ptf.addCash(10000);
+		
+		StrategyRunner runner = new StrategyRunner(ptf,debut,fin,Currency.EUR,MarketDataPrecision.CANDLE);
 		runner.setBasePackage("org.jquant.example.ma");
 		return runner;
 	}
--- src/main/java/org/jquant/example/ma/SimpleStrategy.java
+++ src/main/java/org/jquant/example/ma/SimpleStrategy.java
@@ -5,12 +5,21 @@ import java.util.Map;
 
 import org.jquant.data.Symbols;
 import org.jquant.indicator.SMA;
-import org.jquant.model.Symbol;
+import org.jquant.instrument.Equity;
+import org.jquant.model.IInstrument;
+import org.jquant.model.InstrumentId;
+import org.jquant.order.Order.OrderSide;
 import org.jquant.serie.Candle;
 import org.jquant.serie.Candle.CandleData;
 import org.jquant.strategy.AbstractStrategy;
 import org.jquant.strategy.Strategy;
 
+/**
+ * Deadly simple daily strategy, using a simple moving average 
+ * 
+ * @author patrick.merheb
+ *
+ */
 @Strategy(value="Simple Strategy")
 public class SimpleStrategy extends AbstractStrategy {
 
@@ -19,44 +28,66 @@ public class SimpleStrategy extends AbstractStrategy {
 	/**
 	 * My indicators collection 
 	 */
-	Map<Symbol,SMA> smaList = new HashMap<Symbol,SMA>() ;
-	
+	Map<InstrumentId,SMA> smaList = new HashMap<InstrumentId,SMA>() ;
 	
 	@Override
 	public void init() {
 		
-		/**
+		/*
 		 * Init Indicators 
 		 */
-		for (Symbol s : getUniverse()){
+		for (InstrumentId s : getMarket()){
 			SMA sma = new SMA(getCandleSerie(s), MA_LENGTH, CandleData.CLOSE);
 			smaList.put(s, sma);
 		}
 		
-
 	}
 
 	/**
 	 * The Strategy universe
 	 */
 	@Override
-	public void createUniverse() {
+	public void initMarket() {
 		addInstrument(Symbols.HEINZ);
 		addInstrument(Symbols.IBM);
 
 	}
 
 	@Override
-	public void onCandle(Symbol instrument, Candle candle) {
+	public void onCandle(InstrumentId instrument, Candle candle) {
 		
-		//logger.debug("I've been called for Instrument " + instrument + " with candle " + candle + " at " + candle.getDate());
+		IInstrument i = new Equity(instrument);
+//		System.out.printf(	"name : %1$s \t " +
+//							"date : %2$td-%2$tm-%2$tY \t " +
+//							"close: %3$f \t" +
+//							"sma :  %4$f \n"
+//							, instrument,candle.getDate().toDate(), candle.getClose(),smaList.get(instrument).getValue(candle.getDate()) );
 		
-		System.out.printf(	"name : %1$s \t " +
-							"date : %2$td-%2$tm-%2$tY \t " +
-							"close: %3$f \t" +
-							"sma :  %4$f \n"
-							
-							, instrument,candle.getDate().toDate(), candle.getClose(),smaList.get(instrument).getValue(candle.getDate()) );
+		/*
+		 * If Open moves above SMA then it's a buy signal, ALL IN 
+		 */
+		if (smaList.get(instrument).getValue(candle.getDate())>candle.getOpen()){
+			// Do we have cash 
+			if (portfolio.getCash()>candle.getHigh()){
+				
+				double qty = Math.floor(portfolio.getCash()/candle.getClose());
+				sendMarketOrder(i, OrderSide.BUY, qty, "Achat sur signal SMA");
+				logger.info("Bought "+qty + " of " + instrument.getCode() + " at date " + candle.getDate().toString("dd/MM/yyyy"));
+			}
+		}
+		/*
+		 * If Open moves below SMA then it's a SELL signal, GET OUT of the Market 
+		 */
+		if (smaList.get(instrument).getValue(candle.getDate())<candle.getOpen()){
+			// Do we have a position on instrument i ? 
+			if (portfolio.getPosition(i)>0){
+				
+				double qty = portfolio.getPosition(i);
+				// Sell on low (worst case) 
+				sendMarketOrder(i, OrderSide.SELL, qty, "Vente sur signal SMA");
+				logger.info("Sold "+qty + " of " + instrument.getCode()+ " at date " + candle.getDate().toString("dd/MM/yyyy"));
+			}
+		}
 		
 		
 	}
